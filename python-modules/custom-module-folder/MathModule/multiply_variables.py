a98645259f069648ed632e3f741008170fa7e34d
Akhila Thorpunuri <akhilathorpunuri@gmail.com>
2026-09-04 11:02:52 +0530

    Remove Python modules from main repository

--- python-modules/custom-module-folder/MathModule/multiply_variables.py
+++
@@ -1,3 +0,0 @@
-def multiply_variables(var1=1, var2=1):
-    """Multiply two variables and return the result."""
-    return var1 * var2
--- python-modules/modules/my_package/sub_module.py
+++
@@ -1,8 +0,0 @@
-#
-# Package Sub-Module In Python
-# Author: __author_credits__
-
-
-
-def sub_greet(name):
-    print(f"Hello from the sub-module, {name}!")
